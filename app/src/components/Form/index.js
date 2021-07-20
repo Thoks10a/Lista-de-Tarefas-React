70589542532ffb5c7300afad7f4006070e04b9a9
Thoks10a <nicholas.isola@hotmail.com>
2021-07-20 16:06:50 -0300

    Lista-De-Tarefas

---
+++ app/src/components/Form/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+
+import { FaPlus } from 'react-icons/fa';
+
+import './form.css';
+
+export default function Form({ handleSubmit, inputMudou, novaTarefa }) {
+  return (
+    <form onSubmit={handleSubmit} action="#" className="form">
+      <input onChange={inputMudou} type="text" value={novaTarefa} />
+      <button type="submit">
+        <FaPlus />
+      </button>
+    </form>
+  );
+}
+
+Form.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  inputMudou: PropTypes.func.isRequired,
+  novaTarefa: PropTypes.string.isRequired,
+};
